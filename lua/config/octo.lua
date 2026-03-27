ad7ba6768d3253bfcb0b6d37ed76c1ab00dadf0f
Suhas Narreddy <suhas.narreddy@turing.bio>
2026-03-27 13:00:06 +0100

    feat: wire octo.nvim into plugins, remove diffview

---
+++ lua/config/octo.lua
@@ -0,0 +1,15 @@
+return {
+  "pwntester/octo.nvim",
+  dependencies = {
+    "nvim-lua/plenary.nvim",
+    "nvim-telescope/telescope.nvim",
+    "nvim-tree/nvim-web-devicons",
+  },
+  cmd = { "Octo" },
+  keys = {
+    { "<leader>gr", "<cmd>Octo pr list<cr>", desc = "Octo: List PRs" },
+  },
+  config = function()
+    require("octo").setup()
+  end,
+}
--- lua/plugins.lua
+++ lua/plugins.lua
@@ -18,6 +18,8 @@ vim.opt.rtp:prepend(lazypath)
 require('lazy').setup({
 -- Git related plugins
   require('config.gitplugs'),
+  -- Octo: GitHub PR review
+  require('config.octo'),
 -- Detect tabstop and shiftwidth automatically
   'tpope/vim-sleuth',
 -- Neo Tree
@@ -104,6 +106,23 @@ require('lazy').setup({
     { "<c-s>", mode = { "c" }, function() require("flash").toggle() end, desc = "Toggle Flash Search" },
   },
 },
+  -- Snacks: picker + gh cli integration
+  {
+    "folke/snacks.nvim",
+    priority = 1000,
+    lazy = false,
+    opts = {
+      gh = {},
+      picker = {},
+    },
+    keys = {
+      { "<leader>gi", function() Snacks.picker.gh_issue() end, desc = "GitHub Issues (open)" },
+      { "<leader>gI", function() Snacks.picker.gh_issue({ state = "all" }) end, desc = "GitHub Issues (all)" },
+      { "<leader>gp", function() Snacks.picker.gh_pr() end, desc = "GitHub Pull Requests (open)" },
+      { "<leader>gP", function() Snacks.picker.gh_pr({ state = "all" }) end, desc = "GitHub Pull Requests (all)" },
+    },
+  }
+  ,
 }, {})
 
 vim.g.fugitive_gitlab_domains={['git@gitlab.turing.bio:turing/turingdb.git'] = 'https://gitlab.turing.bio/turing/turingdb.git'}
